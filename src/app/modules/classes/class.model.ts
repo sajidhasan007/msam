d3238681e1c42e8893ebce5dede41b89de1c38e1
sajidhasan007 <sajid.hasan@misfit.tech>
2024-01-31 13:08:02 +0600

    some log add

--- src/app/modules/classes/class.model.ts
+++ src/app/modules/classes/class.model.ts
@@ -1,32 +1,40 @@
 import { Schema, model } from 'mongoose';
 import { ClassesModel, IClasses } from './class.interface';
 
-export const ClassSchema = new Schema<IClasses, ClassesModel>({
-  title: {
-    type: String,
-    required: true,
-  },
-  dateTime: {
-    type: Date,
-    required: true,
-  },
-  isDone: {
-    type: Boolean,
-    default: false,
-  },
-  description: {
-    type: String,
-  },
-
-  students: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Student',
+export const ClassSchema = new Schema<IClasses, ClassesModel>(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-  files: {
-    type: [String],
+    dateTime: {
+      type: Date,
+      required: true,
+    },
+    isDone: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+    },
+
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+    files: {
+      type: [String],
+    },
   },
-});
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 export const Class = model<IClasses, ClassesModel>('Class', ClassSchema);
